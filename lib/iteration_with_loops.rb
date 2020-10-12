57083ad57a8d4e5f400679d1b438afefecde3328
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 23:34:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,18 @@ def find_min_in_nested_arrays(src)
   row_index = 0 
   while row_index < src.count do 
     element_index = 0 
-    min_temp = 0 
-    while 
-end
+    min_temp = Float:: INFINITY
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index] < min_temp
+        min_temp = src[row_index][element_index]
+      end 
+      element_index += 1 
+    end 
+    outer_results << min_temp 
+    row_index += 1 
+  end
+outer_results
+end 
 
 
 # def find_max_value(array)
